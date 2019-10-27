e641746a8b4c9c69b0d28aa2e3cf863e935c6a98
J M <jessmichaud488@gmail.com>
2019-10-27 19:01:20 -0400

    moved delete button into past entries

--- src/Form.js
+++ src/Form.js
@@ -48,15 +48,16 @@ class Form extends React.Component {
                 onChange={this.props.changeEntryHandler}
             />
             <br></br>
-            <button type="submit" onClick={this.props.handleDeleteEntry}>Delete</button>
             <button type="submit" onClick={this.props.handleFormClick}>Submit</button>
 
         <ul>
             <div id="pastEntries">
             <h1 className="gradient">Past Entries</h1>
+            <h2>Click the entry's title to view and edit!</h2>
             {this.props.entryArray.map((item, i) => (
             <li onClick={this.props.handleLiClick} data-id={i} value={item.id} key={i}>{item.title}</li>
           ))}
+          <button type="submit" onClick={this.props.handleDeleteEntry}>Delete</button>
           </div>
         </ul>
         
