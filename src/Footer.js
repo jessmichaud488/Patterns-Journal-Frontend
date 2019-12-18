4418b09669fe8223f7309f7b9ee248d91902c277
J M <jessmichaud488@gmail.com>
2019-12-17 19:20:48 -0500

    Reformatted footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,11 +5,12 @@ render () {
     return (
   <div className="button-footer">
   <div className="links">
-    <img className="footer-image" src="images/linkedin-white.png" target="_blank" href="https://www.linkedin.com/in/jessicamichauddevlife/" />
-    <img className="footer-image" src="images/angels-list-white.png" target="_blank" href="https://angel.co/jessica-l-michaud" />
-    <img class="footer-image" src="images/github-cat-image.png" target="_blank" href="https://github.com/jessmichaud488" />
-    <img class="footer-image" src="images/origami-bird-white.png" target="_blank" href="https://jessmichaud488.github.io/" />
+    <a className="footer-image" src="images/linkedin-white.png" target="_blank" href="https://www.linkedin.com/in/jessicamichauddevlife/" />
+    <a className="footer-image" src="images/angels-list-white.png" target="_blank" href="https://angel.co/jessica-l-michaud" />
+    <a class="footer-image" src="images/github-cat-image.png" target="_blank" href="https://github.com/jessmichaud488" />
+    <a class="footer-image" src="images/origami-bird-white.png" target="_blank" href="https://jessmichaud488.github.io/" />
     <div className="overlay">
+      <a>Connect with me!</a>
     </div>
 </div>
 </div>
