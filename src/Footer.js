ace5dba0da3953cc89765250ac72ea4d47bf1c42
J M <jessmichaud488@gmail.com>
2019-12-20 12:17:26 -0500

    fixing images in social media footer

--- src/Footer.js
+++ src/Footer.js
@@ -6,12 +6,12 @@ render () {
 <div id="twitter-circle" class="row">
   <div class="col-xs-6">
       <div id="tw-3" class="tw-outside">
-          <div class="tw-text"><span><img src="src/images/origami-bird-white.png" /></span></div>
+          <div class="tw-text"><span><img src="images/origami-bird-white.png" /></span></div>
           <div class="tw-click">
-              <a href="" class="twitter-follow-button" data-show-count="false"><img src="src/images/origami-bird-white" /></a>
-              <a href="" class="twitter-follow-button" data-show-count="false"><img src="src/images/github-cat-white.png" /></a>
-              <a href="" class="twitter-follow-button" data-show-count="false"><img src="src/images/linkedin-white" /></a>
-              <a href="" class="twitter-follow-button" data-show-count="false"><img src="src/images/angels-list-white.png" /></a>
+              <a href="" class="twitter-follow-button" data-show-count="false"><img src="images/origami-bird-white" /></a>
+              <a href="" class="twitter-follow-button" data-show-count="false"><img src="images/github-cat-white.png" /></a>
+              <a href="" class="twitter-follow-button" data-show-count="false"><img src="images/linkedin-white" /></a>
+              <a href="" class="twitter-follow-button" data-show-count="false"><img src="images/angels-list-white.png" /></a>
           </div>
       </div>
   </div>
