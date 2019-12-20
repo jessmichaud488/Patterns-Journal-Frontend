dd4f1ca038fb4dd7dbafc56e180f0092e22cfe8c
J M <jessmichaud488@gmail.com>
2019-12-20 12:56:23 -0500

    Footer image test

--- src/Footer.js
+++ src/Footer.js
@@ -6,12 +6,12 @@ render () {
 <div id="twitter-circle" class="row">
   <div class="col-xs-6">
       <div id="tw-3" class="tw-outside">
-          <div class="tw-text"><span><img src="images/origami-bird-white.png" /></span></div>
+          <div class="tw-text"><span src="images/origami-bird-white.png"></span></div>
           <div class="tw-click">
-              <a href="" class="twitter-follow-button" data-show-count="false"><img src="images/origami-bird-white.png" /></a>
-              <a href="" class="twitter-follow-button" data-show-count="false"><img src="images/github-cat-white.png" /></a>
-              <a href="" class="twitter-follow-button" data-show-count="false"><img src="images/linkedin-white.png" /></a>
-              <a href="" class="twitter-follow-button" data-show-count="false"><img src="images/angels-list-white.png" /></a>
+              <a href="https://jessmichaud488.github.io/" class="twitter-follow-button" data-show-count="false"><img src="images/origami-bird-white.png" /></a>
+              <a href="https://github.com/jessmichaud488" class="twitter-follow-button" data-show-count="false"><img src="images/github-cat-white.png" /></a>
+              <a href="https://www.linkedin.com/in/jessicamichauddevlife/" class="twitter-follow-button" data-show-count="false"><img src="images/linkedin-white.png" /></a>
+              <a href="https://angel.co/jessica-l-michaud" class="twitter-follow-button" data-show-count="false"><img src="images/angels-list-white.png" /></a>
           </div>
       </div>
   </div>
