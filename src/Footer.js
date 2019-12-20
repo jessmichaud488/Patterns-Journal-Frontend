769670c5091ac06ce2b6f776d8724f8e05a6c194
J M <jessmichaud488@gmail.com>
2019-12-20 14:06:37 -0500

    Fixed typo

--- src/Footer.js
+++ src/Footer.js
@@ -3,15 +3,15 @@ import React from 'react';
 class Footer extends React.Component {
 render () {
     return (
-<div id="twitter-circle" nameClass="row">
+<div id="twitter-circle" className="row">
   <div>
-      <div id="tw-3" nameClass="tw-outside">
-          <div nameClass="tw-text"><img src="images/origami-bird-white.png" nameClass="icon-button" /></div>
-          <div nameClass="tw-click">
-              <a href="https://jessmichaud488.github.io/"><img src="images/origami-bird-white.png" nameClass="icon-button" /></a>
-              <a href="https://github.com/jessmichaud488"><img src="images/github-cat-white.png" nameClass="icon-button" /></a>
-              <a href="https://www.linkedin.com/in/jessicamichauddevlife/"><img src="images/linkedin-white.png" nameClass="icon-button" /></a>
-              <a href="https://angel.co/jessica-l-michaud"><img src="images/angels-list-white.png" nameClass="icon-button" /></a>
+      <div id="tw-3" className="tw-outside">
+          <div className="tw-text"><img src="images/origami-bird-white.png" className="icon-button" /></div>
+          <div className="tw-click">
+              <a href="https://jessmichaud488.github.io/"><img src="images/origami-bird-white.png" className="icon-button" /></a>
+              <a href="https://github.com/jessmichaud488"><img src="images/github-cat-white.png" className="icon-button" /></a>
+              <a href="https://www.linkedin.com/in/jessicamichauddevlife/"><img src="images/linkedin-white.png" className="icon-button" /></a>
+              <a href="https://angel.co/jessica-l-michaud"><img src="images/angels-list-white.png" className="icon-button" /></a>
           </div>
       </div>
   </div>
