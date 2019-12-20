b6be83aaf47d6dbb21f530fb0b6434f17772ab2f
J M <jessmichaud488@gmail.com>
2019-12-20 14:13:12 -0500

    Footer icon styling

--- src/Footer.js
+++ src/Footer.js
@@ -8,10 +8,10 @@ render () {
       <div id="tw-3" className="tw-outside">
           <div className="tw-text"><img src="images/origami-bird-white.png" className="icon-button" /></div>
           <div className="tw-click">
-              <a href="https://jessmichaud488.github.io/"><img src="images/origami-bird-white.png" className="icon-button" /></a>
-              <a href="https://github.com/jessmichaud488"><img src="images/github-cat-white.png" className="icon-button" /></a>
-              <a href="https://www.linkedin.com/in/jessicamichauddevlife/"><img src="images/linkedin-white.png" className="icon-button" /></a>
-              <a href="https://angel.co/jessica-l-michaud"><img src="images/angels-list-white.png" className="icon-button" /></a>
+              <a target="_blank" href="https://jessmichaud488.github.io/"><img src="images/origami-bird-white.png" className="icon-button" alt="White portfolio logo" /></a>
+              <a target="_blank" ref="https://github.com/jessmichaud488"><img src="images/github-cat-white.png" className="icon-button" alt="White Github logo" /></a>
+              <a target="_blank" href="https://www.linkedin.com/in/jessicamichauddevlife/"><img src="images/linkedin-white.png" className="icon-button" alt="White LinkedIn logo" /></a>
+              <a target="_blank" href="https://angel.co/jessica-l-michaud"><img src="images/angels-list-white.png" className="icon-button" alt="White Angel's List logo" /></a>
           </div>
       </div>
   </div>
