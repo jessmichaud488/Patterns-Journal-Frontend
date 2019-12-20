ac01135f246ee9491ba7261556beecd00fcb6bc1
J M <jessmichaud488@gmail.com>
2019-12-20 13:53:54 -0500

    Footer icon styling

--- src/Footer.js
+++ src/Footer.js
@@ -8,10 +8,10 @@ render () {
       <div id="tw-3" class="tw-outside">
           <div class="tw-text"><span src="images/origami-bird-white.png"></span></div>
           <div class="tw-click">
-              <a href="https://jessmichaud488.github.io/" class="twitter-follow-button"><span src="images/origami-bird-white.png"></span></a>
-              <a href="https://github.com/jessmichaud488" class="twitter-follow-button"><span src="images/github-cat-white.png"></span></a>
-              <a href="https://www.linkedin.com/in/jessicamichauddevlife/" class="twitter-follow-button"><span src="images/linkedin-white.png"></span></a>
-              <a href="https://angel.co/jessica-l-michaud" class="twitter-follow-button"><span src="images/angels-list-white.png"></span></a>
+              <a href="https://jessmichaud488.github.io/" class="icon-button"><span src="images/origami-bird-white.png"></span></a>
+              <a href="https://github.com/jessmichaud488" class="icon-button"><span src="images/github-cat-white.png"></span></a>
+              <a href="https://www.linkedin.com/in/jessicamichauddevlife/" class="icon-button"><span src="images/linkedin-white.png"></span></a>
+              <a href="https://angel.co/jessica-l-michaud" class="icon-button"><span src="images/angels-list-white.png"></span></a>
           </div>
       </div>
   </div>
