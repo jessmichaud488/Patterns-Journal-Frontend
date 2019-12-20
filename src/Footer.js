c04f0dfa9dc948c3c06623f3028cf6b66885429e
J M <jessmichaud488@gmail.com>
2019-12-20 12:27:13 -0500

    footer image test

--- src/Footer.js
+++ src/Footer.js
@@ -8,9 +8,9 @@ render () {
       <div id="tw-3" class="tw-outside">
           <div class="tw-text"><span><img src="images/origami-bird-white.png" /></span></div>
           <div class="tw-click">
-              <a href="" class="twitter-follow-button" data-show-count="false"><img src="images/origami-bird-white" /></a>
+              <a href="" class="twitter-follow-button" data-show-count="false"><img src="images/origami-bird-white.png" /></a>
               <a href="" class="twitter-follow-button" data-show-count="false"><img src="images/github-cat-white.png" /></a>
-              <a href="" class="twitter-follow-button" data-show-count="false"><img src="images/linkedin-white" /></a>
+              <a href="" class="twitter-follow-button" data-show-count="false"><img src="images/linkedin-white.png" /></a>
               <a href="" class="twitter-follow-button" data-show-count="false"><img src="images/angels-list-white.png" /></a>
           </div>
       </div>
