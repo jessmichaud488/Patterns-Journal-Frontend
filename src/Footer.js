f789b041cd789a200df37cbfb8f01cb870eeec40
J M <jessmichaud488@gmail.com>
2019-12-20 14:30:28 -0500

    Removed z-index

--- src/Footer.js
+++ src/Footer.js
@@ -9,7 +9,7 @@ render () {
           <div className="tw-text"><img src="src/images/origami-bird-white.png" className="icon-button" alt="White portfolio logo" /></div>
           <div className="tw-click">
               <a target="_blank" href="https://jessmichaud488.github.io/"><img src="images/origami-bird-white.png" className="icon-button" alt="White portfolio logo" /></a>
-              <a target="_blank" ref="https://github.com/jessmichaud488"><img src="images/github-cat-white.png" className="icon-button" alt="White Github logo" /></a>
+              <a target="_blank" href="https://github.com/jessmichaud488"><img src="images/github-cat-white.png" className="icon-button" alt="White Github logo" /></a>
               <a target="_blank" href="https://www.linkedin.com/in/jessicamichauddevlife/"><img src="images/linkedin-white.png" className="icon-button" alt="White LinkedIn logo" /></a>
               <a target="_blank" href="https://angel.co/jessica-l-michaud"><img src="images/angels-list-white.png" className="icon-button" alt="White Angel's List logo" /></a>
           </div>
