e923cb290c1ab7e15e2704331caa2604ff551849
J M <jessmichaud488@gmail.com>
2019-12-20 11:56:14 -0500

    Footer test

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ render () {
 <div id="twitter-circle" class="row">
   <div class="col-xs-6">
       <div id="tw-3" class="tw-outside">
-          <div class="tw-text"><span class="fa fa-twitter"></span></div>
+          <div class="tw-text"><span img="images/origami-bird-white.png"></span></div>
           <div class="tw-click">
               <a href="https://twitter.com/scotch_io" class="twitter-follow-button" data-show-count="false">Follow @scotch_io</a>
           </div>
