a9570de519617fd209d60b49ba099f9acf25ac1d
J M <jessmichaud488@gmail.com>
2019-12-20 14:02:51 -0500

    Fixed component structure

--- src/Footer.js
+++ src/Footer.js
@@ -3,15 +3,15 @@ import React from 'react';
 class Footer extends React.Component {
 render () {
     return (
-<div id="twitter-circle" class="row">
-  <div class="col-xs-6">
-      <div id="tw-3" class="tw-outside">
-          <div class="tw-text"><span src="images/origami-bird-white.png"></span></div>
-          <div class="tw-click">
-              <a href="https://jessmichaud488.github.io/" class="icon-button"><span><img src="images/origami-bird-white.png" /></span></a>
-              <a href="https://github.com/jessmichaud488" class="icon-button"><span><img src="images/github-cat-white.png" /></span></a>
-              <a href="https://www.linkedin.com/in/jessicamichauddevlife/" class="icon-button"><span><img src="images/linkedin-white.png" /></span></a>
-              <a href="https://angel.co/jessica-l-michaud" class="icon-button"><span><img src="images/angels-list-white.png" /></span></a>
+<div id="twitter-circle" nameClass="row">
+  <div>
+      <div id="tw-3" nameClass="tw-outside">
+          <div nameClass="tw-text"><img src="images/origami-bird-white.png" nameClass="icon-button" /></div>
+          <div nameClass="tw-click">
+              <a href="https://jessmichaud488.github.io/"><img src="images/origami-bird-white.png" nameClass="icon-button" /></a>
+              <a href="https://github.com/jessmichaud488"><img src="images/github-cat-white.png" nameClass="icon-button" /></a>
+              <a href="https://www.linkedin.com/in/jessicamichauddevlife/"><img src="images/linkedin-white.png" nameClass="icon-button" /></a>
+              <a href="https://angel.co/jessica-l-michaud"><img src="images/angels-list-white.png" nameClass="icon-button" /></a>
           </div>
       </div>
   </div>
